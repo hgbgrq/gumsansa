7138d622b719bbdd0bd1d77a7aa2a66f2bde763c
hgbgrq <57474913+hgbgrq@users.noreply.github.com>
2022-11-30 08:45:06 +0900

    발주 리스트

--- src/main/java/com/example/gumsansa/ordering/dto/res/OrderReadListDto.java
+++ src/main/java/com/example/gumsansa/ordering/dto/res/OrderReadListDto.java
@@ -1,4 +1,13 @@
 package com.example.gumsansa.ordering.dto.res;
 
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class OrderReadListDto {
+
+    private List<OrderReadResDto> orderReadResDtoList;
+
 }
